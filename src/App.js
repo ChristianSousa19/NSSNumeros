1ccc4e8a99a1ec84f8ffdb08e205ecf89a758483
ChristianSousa19 <christianssv37@gmail.com>
2025-03-27 23:16:41 -0300

    Atualizações no projeto, incluindo novos assets e modificações na barra lateral

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/SideBar/SideBar";
 import Home from "./pages/Home/Home";
 import Sobre from "./pages/Sobre/Sobre";
@@ -26,48 +26,114 @@ import Investimento from "./pages/Governança-e-Gestao/Investimento-de-TI/Invest
 import Gerenciamento from "./pages/Governança-e-Gestao/Gerenciamento-de-Serviços/Gerenciamento";
 import ASG from "./pages/Governança-e-Gestao/ASG/ASG";
 import Tecnologias from "./pages/Tecnologias Disruptivas/Tecnologias";
+import "./App.css"; // Importa o CSS
 
 const App = () => {
   return (
     <Router>
-      <Sidebar open={true} />
-      <Routes>
-        {/* Páginas principais */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/Sobre" element={<Sobre />} />
-        <Route path="/Contato" element={<Contato />} />
+      {/* Container principal usando display: flex */}
+      <div className="app-container">
+        <Sidebar open={true} />
+        <div className="app-content">
+          <Routes>
+            {/* Páginas principais */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/Sobre" element={<Sobre />} />
+            <Route path="/Contato" element={<Contato />} />
 
-        {/* Transações de TI */}
-        <Route path="/transacoes-de-TI/processamento-e-armazenamento/Processamento" element={<Processamento />} />
-        <Route path="/transacoes-de-TI/processamento-e-armazenamento/Pagina2" element={<Pagina2 />} />
-        <Route path="/transacoes-de-TI/conectividade/Conectividade" element={<Conectividade />} />
-        <Route path="/transacoes-de-TI/Atendimento/Atendimento" element={<Atendimento />} />
-        <Route path="/transacoes-de-TI/Ambiente de Trabalho/ambiente" element={<Ambiente />} />
-        <Route path="/transacoes-de-TI/Colaboraçao/Colaboracao" element={<Colaboracao />} />
-        <Route path="/transacoes-de-TI/Telefonia/Telefonia" element={<Telefonia />} />
+            {/* Transações de TI */}
+            <Route
+              path="/transacoes-de-TI/processamento-e-armazenamento/Processamento"
+              element={<Processamento />}
+            />
+            <Route
+              path="/transacoes-de-TI/processamento-e-armazenamento/Pagina2"
+              element={<Pagina2 />}
+            />
+            <Route
+              path="/transacoes-de-TI/conectividade/Conectividade"
+              element={<Conectividade />}
+            />
+            <Route
+              path="/transacoes-de-TI/Atendimento/Atendimento"
+              element={<Atendimento />}
+            />
+            <Route
+              path="/transacoes-de-TI/Ambiente de Trabalho/ambiente"
+              element={<Ambiente />}
+            />
+            <Route
+              path="/transacoes-de-TI/Colaboraçao/Colaboracao"
+              element={<Colaboracao />}
+            />
+            <Route
+              path="/transacoes-de-TI/Telefonia/Telefonia"
+              element={<Telefonia />}
+            />
 
-        {/* Grandes Movimentos */}
-        <Route path="/Grandes movimentos/Nuvem/Nuvem" element={<Nuvem />} />
-        <Route path="/Grandes movimentos/PIX/Pix" element={<Pix />} />
-        <Route path="/Grandes movimentos/BAAS/BAAS" element={<BASS />} />
-        <Route path="/Grandes movimentos/Low Code/lowcode" element={<Lowcode />} />
-        <Route path="/Grandes movimentos/Canais Digitais/Canais" element={<Canais />} />
-        <Route path="/Grandes movimentos/Canais Digitais/CanaisPF" element={<CanaisPF />} />
-        <Route path="/Grandes movimentos/Canais Digitais/CanaisPJ" element={<CanaisPJ />} />
-        <Route path="/Grandes movimentos/Canais Digitais/Whatsapp" element={<Whtsapp />} />
+            {/* Grandes Movimentos */}
+            <Route
+              path="/Grandes movimentos/Nuvem/Nuvem"
+              element={<Nuvem />}
+            />
+            <Route path="/Grandes movimentos/PIX/Pix" element={<Pix />} />
+            <Route path="/Grandes movimentos/BAAS/BAAS" element={<BASS />} />
+            <Route
+              path="/Grandes movimentos/Low Code/lowcode"
+              element={<Lowcode />}
+            />
+            <Route
+              path="/Grandes movimentos/Canais Digitais/Canais"
+              element={<Canais />}
+            />
+            <Route
+              path="/Grandes movimentos/Canais Digitais/CanaisPF"
+              element={<CanaisPF />}
+            />
+            <Route
+              path="/Grandes movimentos/Canais Digitais/CanaisPJ"
+              element={<CanaisPJ />}
+            />
+            <Route
+              path="/Grandes movimentos/Canais Digitais/Whatsapp"
+              element={<Whtsapp />}
+            />
 
-        {/* Governança e Gestão */}
-        <Route path="/Governança-e-Gestao/Riscos/Riscos" element={<Riscos />} />
-        <Route path="/Governança-e-Gestao/Premiaçoes-e-certificaçoes/Premiacoes" element={<Premiacoes />} />
-        <Route path="/Governança-e-Gestao/Portifolio/Portifolio" element={<Portifolio />} />
-        <Route path="/Governança-e-Gestao/Pessoas/Pessoas" element={<Pessoas />} />
-        <Route path="/Governança-e-Gestao/Investimento-de-TI/Investimento" element={<Investimento />} />
-        <Route path="/Governança-e-Gestao/Gerenciamento-de-Servicos/Gerenciamento" element={<Gerenciamento />} />
-        <Route path="/Governança-e-Gestao/ASG/ASG" element={<ASG />} />
+            {/* Governança e Gestão */}
+            <Route
+              path="/Governança-e-Gestao/Riscos/Riscos"
+              element={<Riscos />}
+            />
+            <Route
+              path="/Governança-e-Gestao/Premiaçoes-e-certificaçoes/Premiacoes"
+              element={<Premiacoes />}
+            />
+            <Route
+              path="/Governança-e-Gestao/Portifolio/Portifolio"
+              element={<Portifolio />}
+            />
+            <Route
+              path="/Governança-e-Gestao/Pessoas/Pessoas"
+              element={<Pessoas />}
+            />
+            <Route
+              path="/Governança-e-Gestao/Investimento-de-TI/Investimento"
+              element={<Investimento />}
+            />
+            <Route
+              path="/Governança-e-Gestao/Gerenciamento-de-Servicos/Gerenciamento"
+              element={<Gerenciamento />}
+            />
+            <Route path="/Governança-e-Gestao/ASG/ASG" element={<ASG />} />
 
-        {/* Tecnologias Disruptivas */}
-        <Route path="/Tecnologias Disruptivas/Tecnologias" element={<Tecnologias />} />
-      </Routes>
+            {/* Tecnologias Disruptivas */}
+            <Route
+              path="/Tecnologias Disruptivas/Tecnologias"
+              element={<Tecnologias />}
+            />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
