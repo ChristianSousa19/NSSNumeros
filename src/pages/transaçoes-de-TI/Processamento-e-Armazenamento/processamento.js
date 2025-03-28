ee28f1b01f248b6e8a60f7f7bfd95260b02cd841
Oliveira <isabele.oliveira@capgemini.com>
2025-03-28 17:19:29 -0300

    Criando a branch transações de ti  e fazendo alterações

--- src/pages/transaçoes-de-TI/Processamento-e-Armazenamento/processamento.js
+++ src/pages/transaçoes-de-TI/Processamento-e-Armazenamento/processamento.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import "./Processamento.css"; 
 
 const Processamento = () => {
   return (
-    <div>
-      <h1>processamento</h1>
-      <p>Bem-vindo à página de processamento. Aqui você pode gerenciar todas as informações relacionadas aos processamento.</p>
+    <section className='ContainerProcessamento'>
+      
+    <div className='ProcessamentoDIV'>
+      <h1 className='Processamentoh1'>Servidores High-end</h1>
+      
       {/* Adicione mais conteúdo ou componentes conforme necessário */}
     </div>
+    <div className='ProcessamentoDIV'>
+    <h1 className='Processamentoh1'>Capacidade versus volume de dados Mainframe e High-end(PB)</h1>
+
+    </div>
+    <div className='ProcessamentoDIV'>
+      <h1 className='Processamentoh1'>Transações Mainframe</h1>
+      
+      {/* Adicione mais conteúdo ou componentes conforme necessário */}
+    </div>
+    <div className='ProcessamentoDIV'>
+      <h1 className='Processamentoh1'>Transações no Dia de Maior Pico</h1>
+      
+      {/* Adicione mais conteúdo ou componentes conforme necessário */}
+    </div>
+    
+    </section>
   );
 };
 
